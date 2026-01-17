f7ffb1e13ca6f8749a98208f67b0b43876efb4f7
vishugt143 <158712030+vishugt143@users.noreply.github.com>
2026-01-17 13:32:04 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -6,15 +6,15 @@ from os import path, getenv
 class Config:
     API_ID = int(getenv("API_ID", "21419016"))
     API_HASH = getenv("API_HASH", "79198e1eb4cfd0f771a89d83b9144e7e")
-    BOT_TOKEN = getenv("BOT_TOKEN", "8455395512:AAGbNe2nv9_ztC685urGaGLIeDH5hQdJIRQ")
+    BOT_TOKEN = getenv("BOT_TOKEN", "8143661076:AAHcDJGX-at8rMAhMnTV8JZZZOKpMlyNLBw")
     # Your Force Subscribe Channel Id Below=
-    CHID = int(getenv("CHID", "-1003431327885")) # Make Bot Admin In This Channel
+    CHID = int(getenv("CHID", "-1002425340898")) # Make Bot Admin In This Channel
     # Admin Or Owner Id |
-    SUDO = list(map(int, getenv("SUDO", "7554081592 7001232146 7564050858 5656436152").split()))
+    SUDO = list(map(int, getenv("SUDO", "7554081592 6483216200 7564050858 5656436152").split()))
     MONGO_URI = getenv("MONGO_URI", "mongodb+srv://melodysotto4_db_user:BCUKIKDEAqFEzeCj@cluster0.trrt89o.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     POSTS = [
-        "https://t.me/forward_hack_lnx/3",
-        "https://t.me/forward_hack_lnx/4"
+        "https://t.me/forward_hack_lnx/7",
+        "https://t.me/forward_hack_lnx/6"
     ]
 
 cfg = Config()
